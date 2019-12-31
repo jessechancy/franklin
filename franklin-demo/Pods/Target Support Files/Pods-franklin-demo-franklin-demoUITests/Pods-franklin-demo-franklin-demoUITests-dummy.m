b3aa9b572d88831db893e5467fdf479a0d0ef9ba
jzchan34 <jessec1@andrew.cmu.edu>
2019-12-31 17:34:42 +0800

    Integrated Gitlab

--- franklin-demo/Pods/Target Support Files/Pods-franklin-demo-franklin-demoUITests/Pods-franklin-demo-franklin-demoUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_franklin_demo_franklin_demoUITests : NSObject
-@end
-@implementation PodsDummy_Pods_franklin_demo_franklin_demoUITests
-@end
